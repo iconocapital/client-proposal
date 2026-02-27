0ebfa9828b83a353364e393ccb029e1fb36f0ce0
iconocapital <chris@iconocapital.com>
2026-02-26 21:37:02 -0500

    Change base path from '/alinger-proposal/' to '/client-proposal/'

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/alinger-proposal/",
+    base: "/client-proposal/",
   server: {
     host: "::",
     port: 8080,
